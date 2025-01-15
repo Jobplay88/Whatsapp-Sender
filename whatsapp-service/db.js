bee5f97a9d13d765e86b2bb0a8b4b91326196791
Sunnyyy1998 <sunny.public98@gmail.com>
2025-01-15 09:44:29 +0800

    feat: update db.js

--- whatsapp-service/db.js
+++ whatsapp-service/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: process.env.HOST, // Replace with your DB host
-    user: process.env.USER,      // Replace with your DB user
-    password: process.env.PASSWORD, // Replace with your DB password
-    database: process.env.DATABASE, // Replace with your DB name
+    host: process.env.DB_HOST, // Replace with your DB host
+    user: process.env.DB_USER,      // Replace with your DB user
+    password: process.env.DB_PASSWORD, // Replace with your DB password
+    database: process.env.DB_DATABASE, // Replace with your DB name
     connectionLimit: 10 // Optional for connection pooling
 });
 
