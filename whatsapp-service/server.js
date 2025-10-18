e489a57a40fac3f034ea19a2fb6706fdb0223224
Sunnyyy1998 <sunny.public98@gmail.com>
2025-10-18 17:01:32 +0800

    feat: new whatsapp log into db

--- whatsapp-service/server.js
+++ whatsapp-service/server.js
@@ -20,10 +20,10 @@ const upload = multer({ dest: 'uploads/' });
 const striptags = require('striptags');
 
 // Read SSL certificate files
-// const credentials = {
-//     key: fs.readFileSync('/home/ubuntu/ssl/privkey.pem'),
-//     cert: fs.readFileSync('/home/ubuntu/ssl/fullchain.pem'),
-// };
+const credentials = {
+    key: fs.readFileSync('/home/ubuntu/ssl/privkey.pem'),
+    cert: fs.readFileSync('/home/ubuntu/ssl/fullchain.pem'),
+};
 
 // Enable CORS for API requests (Express)
 app.use(cors({
@@ -60,8 +60,8 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 // app.use(ipWhitelistMiddleware);
 
 // Create HTTP server and integrate Socket.IO
-const server = http.createServer(app);
-// const server = https.createServer(credentials, app);
+// const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 
 // Enable CORS for WebSocket (Socket.IO)
 const io = new Server(server, {
